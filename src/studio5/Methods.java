907904d456a4ae7c7c414217266e762d64585988
1230e <1230e@DESKTOP-EN6GDDG.dhcp.wustl.edu>
2022-10-19 15:41:08 -0500

    hi

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -90,7 +90,9 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+		for (int i = 0; i< values.length; i++ ) {
+			sum = values[i]+sum;
+		}
 		return sum;
 	}
 
